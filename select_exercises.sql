ff5de836dd592ffcb497661a1da5152acc59a95c
JenniferLA <park.jennifer2324@gmail.com>
2023-11-28 10:08:31 -0600

    completed update_exercises.sql exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,11 +3,11 @@ use hippo_jenn;
 -- The name of all albums by Pink Floyd:
     select * from albums where artist = 'Pink Floyd';
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released:
-    select release_date from albums where name = 'Sgt. Pepper'/s' Lonely Hearts Club';
+    select release_date from albums where name = 'The Bodyguard';
 -- The genre for Nevermind:
-    select genre from albums where name = 'Nevermind';
+    select genre from albums where name = 'Thriller';
 -- Which albums were released in the 1990s:
-    select * from albums where release_date = 1990;
+    select * from albums where release_date = 1977;
 -- Which albums had less than 20 million certified sales:
     SELECT * FROM albums WHERE sales > 20;
 -- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
---
+++ update_exercises.sql
@@ -0,0 +1,22 @@
+use hippo_jenn;
+
+-- Write SELECT statements to output each of the following with a caption:
+-- All albums in your table
+    --select * FROM albums AS 'All albums in your table';
+
+-- After each SELECT add an UPDATE statement to:
+    -- Make all the albums 10 times more popular (sales * 10)
+    UPDATE albums SET sales = sales * 10;
+    SELECT * FROM albums;
+    SELECT sales FROM albums;
+    -- Move all the albums before 1980 back to the 1800s.
+    UPDATE albums
+    SET release_date = '1800-01-01'
+    WHERE release_date < '1980-01-01';
+    SELECT release_date FROM albums;
+
+-- Change 'Michael Jackson' to 'Peter Jackson'
+    UPDATE albums
+    SET artist = 'Peter Jackson'
+    WHERE artist = 'Michael Jackson';
+    SELECT artist FROM albums;
