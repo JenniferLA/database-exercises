e83fb6fa7976eac98f4b029638d20a6dadf8d18f
JenniferLA <park.jennifer2324@gmail.com>
2023-11-29 11:50:08 -0600

    completed LIMIT exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,19 +2,31 @@ use employees;
 
     -- ORDER BY EXERCISE
 -- Modify your first query to order by first name. The first result should be Irena Flexer and the last result should be Vidya Awdeh
+--SELECT first_name, last_name
+--FROM employees
+--ORDER BY first_name IN ('Irena') AND last_name IN ('Flexer') DESC, first_name IN ('Vidya') AND last_name IN ('Awdeh') ASC;
 SELECT first_name, last_name
 FROM employees
-ORDER BY first_name IN ('Irena') AND last_name IN ('Flexer') DESC, first_name IN ('Vidya') AND last_name IN ('Awdeh') ASC;
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
 
 -- Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+--SELECT first_name, last_name
+--FROM employees
+--ORDER BY first_name IN ('Irena') AND last_name IN ('Acton') DESC, first_name IN ('Vidya') AND last_name IN ('Zweizig') ASC;
 SELECT first_name, last_name
 FROM employees
-ORDER BY first_name IN ('Irena') AND last_name IN ('Acton') DESC, first_name IN ('Vidya') AND last_name IN ('Zweizig') ASC;
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
 
 -- Change the ORDER BY clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
-SELECT last_name, first_name
+--SELECT last_name, first_name
+--FROM employees
+--ORDER BY first_name IN ('Irena') AND last_name IN ('Acton') DESC, first_name IN ('Maya') AND last_name IN ('Zyda') ASC;
+SELECT first_name, last_name
 FROM employees
-ORDER BY first_name IN ('Irena') AND last_name IN ('Acton') DESC, first_name IN ('Maya') AND last_name IN ('Zyda') ASC;
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
 
 -- Update your queries for employees with 'e' in their last name to sort the results by their employee number. Make sure the employee numbers are in the correct order.
 SELECT emp_no, last_name
